ad2d7d8c712d03d13ca165ab7053fa35e48c7810
janhaviDadhania <janhavivdadhania@gmail.com>
2021-03-13 23:25:34 +0530

    7

--- open.c
+++ open.c
@@ -113,13 +113,15 @@ int common_open(char path[PATH_MAX], int oflags, mode_t omode)
 	vp = new_node(&resolve, oflags, omode);
 	r = err_code;
 
-struct vmnt *v_mp;
-if (r == OK) {exist = FALSE;
-v_mp = find_vmnt(vp->v_fs_e);
-if(strcmp(v_mp->m_mount_path, "/home") == 0) {
-printf("file created: %llu\n", vp->v_inode_nr);
-}
-}
+	// Create File
+	struct vmnt *v_mp;
+	if (r == OK) {
+		exist = FALSE;	/* We just created the file */
+		v_mp = find_vmnt(vp->v_fs_e);	
+		if(strcmp(v_mp->m_mount_path, "/home") == 0) {
+			printf("<minix3> file created: %llu\n", vp->v_inode_nr);
+		}
+	}
 	else if (r != EEXIST) {		/* other error */
 		if (vp) unlock_vnode(vp);
 		unlock_filp(filp);
